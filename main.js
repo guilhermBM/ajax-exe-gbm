4370533772e7adcab1ff237ccf265be527e855f5
guilherme <guilherme@teste.com>
2025-02-02 09:24:00 -0300

    adicionando os arquivos do exercicio

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //Adicionando os conteudos às constantes criadas para recuperar os valores de acordo. 
+
+    const avatarContent = document.querySelector('#avatar');
+    const nameContent = document.querySelector('#name');
+    const usernameContent = document.querySelector('#username');
+    const reposContent = document.querySelector('#repos');
+    const followersContent = document.querySelector('#followers');
+    const followingContent = document.querySelector('#following');
+    const linkContent = document.querySelector('#link')
+
+    //recuperando os valores das informarções necessárias.
+
+    fetch('https://api.github.com/users/guilhermBM')
+        .then(function(resposta) {
+            return resposta.json();
+        })
+        .then(function(json) {
+            avatarContent.src = json.avatar_url;
+            nameContent.innerHTML = json.name;
+            usernameContent.innerHTML = json.login;
+            reposContent.innerHTML = json.public_repos;
+            followersContent.innerHTML = json.followers;
+            followingContent.innerHTML = json.following;
+            linkContent.href = json.html_url;
+        })
+})
